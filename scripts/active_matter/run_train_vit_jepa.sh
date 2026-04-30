359a63d89a5a9e3a8787ad53909105761dacadf7
Ethan <ethanhuang03@gmail.com>
2026-04-30 14:30:33 -0400

    update scripts

--- scripts/active_matter/run_train_vit_jepa.sh
+++ scripts/active_matter/run_train_vit_jepa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source "/scratch/eh3648/physical-representation-learning/scripts/env_setup.sh"
+source "$(dirname "$0")/../env_setup.sh"
 
 torchrun --nproc_per_node=1 --standalone \
     -m physics_jepa.train_vit_jepa \
